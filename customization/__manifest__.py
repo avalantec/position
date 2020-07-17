09c6339163d6a9c2ab884ea8b3f35426e0b23bec
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2020-07-17 11:56:33 -0600

    Update __manifest__.py

--- customization/__manifest__.py
+++ customization/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale_management', 'sale_renting', 'purchase'],
+    'depends': ['base','sale_management', 'sale_renting', 'purchase', 'stock'],
 
     # always loaded
     'data': [
