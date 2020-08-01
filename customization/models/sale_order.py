599506895e0bfae3f46fa9960f6bacc19fc572e7
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2020-08-01 15:04:08 -0600

    Added Button PickUP send by Email

--- customization/models/sale_order.py
+++ customization/models/sale_order.py
@@ -7,6 +7,7 @@ log = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+     _description = "Sales Order Custom"
 
     license_plate = fields.Char(string="# de Placa",store = True)
     container = fields.Char(string="# CTR",store = True)
@@ -28,3 +29,34 @@ class SaleOrder(models.Model):
         store=True
     )
     uninstalled_by = fields.Many2one('res.partner', string="Desinstaldo por",store = True)
+    
+    def action_pickup_send(self):
+        self.ensure_one()
+        template_id = self.env['mail.template'].sudo().search([('name','=','Pick UP Order: Send by email')], limit=1).id
+        
+        lang = self.env.context.get('lang')
+
+        template = self.env['mail.template'].browse(template_id)
+        if template.lang:
+            lang = template._render_template(template.lang, 'sale.order', self.ids[0])
+        ctx = {
+            'default_model': 'sale.order',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'custom_layout': "mail.mail_notification_paynow",
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True,
+            'model_description': self.with_context(lang=lang).type_name,
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(False, 'form')],
+            'view_id': False,
+            'target': 'new',
+            'context': ctx,
+        }
